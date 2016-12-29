6fd38715a6c6611d6792be2d443b01668406b272
Dishant Kaushik <dkaushik95@gmail.com>
2016-12-29 10:59:16 +0530

    Fixed rubric : 3. Changed SplashScreen from Thread to Handler

--- Frontend/Anunciar/app/src/main/java/anunciar/dishant/com/anunciar/UI/SplashScreen.java
+++ Frontend/Anunciar/app/src/main/java/anunciar/dishant/com/anunciar/UI/SplashScreen.java
@@ -5,6 +5,7 @@ import android.app.ActivityOptions;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.transition.Fade;
 import android.transition.Transition;
@@ -33,42 +34,39 @@ public class SplashScreen extends Activity {
         getWindow().setExitTransition(exitTransistion);
 
         mSharedPreferences = getSharedPreferences("anunciar.dishant.com.anunciar", MODE_PRIVATE);
-        Thread timerThread = new Thread() {
+        int time = 0;
+        if (mSharedPreferences.getBoolean("isSignedIn", false)) {
+            time = 1000;
+        } else {
+            time = 3000;
+        }
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
             public void run() {
-                try {
-                    if (mSharedPreferences.getBoolean("isSignedIn", false)) {
-                        sleep(1000);
-                    } else {
-                        sleep(3000);
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    if (mSharedPreferences.getBoolean("isSignedIn", false)) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(SplashScreen.this, new Pair<>(findViewById(R.id.imageView), "splash1"));
-                                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                if (mSharedPreferences.getBoolean("isSignedIn", false)) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(SplashScreen.this, new Pair<>(findViewById(R.id.imageView), "splash1"));
+                            Intent intent = new Intent(SplashScreen.this, MainActivity.class);
 
-                                startActivity(intent, activityOptions.toBundle());
-                            }
-                        });
+                            startActivity(intent, activityOptions.toBundle());
+                        }
+                    });
 
-                    } else {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(SplashScreen.this, new Pair<>(findViewById(R.id.imageView), "splash1"));
-                                Intent intent = new Intent(SplashScreen.this, Login.class);
-                                startActivity(intent, activityOptions.toBundle());
-                            }
-                        });
-                    }
+                } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ActivityOptions activityOptions = ActivityOptions.makeSceneTransitionAnimation(SplashScreen.this, new Pair<>(findViewById(R.id.imageView), "splash1"));
+                            Intent intent = new Intent(SplashScreen.this, Login.class);
+                            startActivity(intent, activityOptions.toBundle());
+                        }
+                    });
                 }
             }
-        };
-        timerThread.start();
+        }, time);
     }
 
     @Override
